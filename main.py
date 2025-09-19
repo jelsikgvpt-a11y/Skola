d368417ceeff280126d342c9e2f4d14cbb406fa5
jelsikgvpt-a11y <jelsik.gvpt@gmail.com>
2025-09-19 22:49:47 +0200

    Prvý commit upravený.

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print ("Tomas Jelsik")
-print ("Hello")
+import math
+r = float(input("Zadaj polomer gule:"))
+Obsah = 4 * math.pi * r**2
+Objem = (4/3) * math.pi * r**3
+print("Obsah je:", Obsah)
+print("Objem je:", Objem)
+input("Stlač Enter pre ukončenie...")
