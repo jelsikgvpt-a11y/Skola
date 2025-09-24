d47eec5b90c32e8d14572199a8d41ebebeb731d5
jelsikgvpt-a11y <jelsik.gvpt@gmail.com>
2025-09-24 09:25:31 +0200

    cisla zoradenie

---
+++ cisla.py
@@ -0,0 +1,19 @@
+a = int(input("Zadaj číslo a: "))
+b = int(input("Zadaj číslo b: "))
+c = int(input("Zadaj číslo c: "))
+if a > b and a > c:
+    if b > c:
+        print( a, b, c)
+    else:
+        print(a, c, b)
+elif b > a and b > c:
+    if a > c:
+        print(b, a, c)
+    else:
+        print( b, c, a)
+elif c > a and c > b:
+    if a > b:
+        print(c, a, b)
+    else:
+        print(c, b, a)
+    
